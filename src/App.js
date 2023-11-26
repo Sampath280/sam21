c6ad056604322593db99e16bf0d14cfc0977b314
Sampath Pujari (MINDTREE LIMITED) <v-sapujari@microsoft.com>
2023-11-26 17:10:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// src/App.js
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './templates/Home';
+import About from './templates/About';
+import Contact from './templates/Contact';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/templates/Contact.js
@@ -0,0 +1,24 @@
+// src/templates/Contact.js
+
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>
+        <center>
+          <p>
+            Contact Me.
+          </p>
+          <a
+            href="https://github.com/swarajgosavi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Swaraj Gosavi (GitHub) [@swarajgosavi]
+          </a>
+        </center>
+      </h1>
+    </div>
+  );
+}
